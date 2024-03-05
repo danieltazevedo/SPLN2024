61cbaf896495da4a908f2df7c47b511f5a47a1a3
danieltazevedo <danielt.azevedo@gmail.com>
2024-03-05 15:37:05 +0000

    Fix TPC3

--- TPC3/makepyproject.py
+++ TPC3/makepyproject.py
@@ -1,49 +1,59 @@
+#!/usr/bin/env python3
+"""python makepyproject.py"""
+import jjcli
 import jinja2
 from glob import glob
 import json
 import os
 
-#project name
-nodes = glob("*.py")
-if len(nodes)>=1:
-    name = nodes[0].replace(".py","")
-else:
-    name = input("Modulo?")
-    
-    
-pp = jinja2.Template('''
-[build-system]
-requires = ["flit_core >=3.2,<4"]
-build-backend = "flit_core.buildapi"
+def main():
+    #project name
+    nodes = glob("*.py")
+    if len(nodes)>=1:
+        name = nodes[0].replace(".py","")
+    else:
+        name = input("Modulo?")
+        
+    v = jjcli.qx(f"grep name '{name}'.py")
+    print("debug",len(v))
+    version = "0.0.1"
+    pp = jinja2.Template('''    
+    [build-system]
+    requires = ["flit_core >=3.2,<4"]
+    build-backend = "flit_core.buildapi"
 
-[project]
-name = "{{name}}"
-authors = [
-    {name = "{{autor}}", email = "{{email}}"},
-]
-classifiers = [
-    "License :: OSI Approved :: MIT License",
-]
-requires-python = ">=3.8"
-dynamic = ["version", "description"]
+    [project]
+    name = "{{name}}"
+    authors = [
+        {name = "{{autor}}", email = "{{email}}"},
+    ]
+    version = "{{version}}"
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+    ]
+    requires-python = ">=3.8"
+    dynamic = ["description"]
 
-dependencies = [
-    "jjcli"
-]
+    dependencies = [
+        "jjcli"
+    ]
 
-[project.scripts]
-{{name}} = "{{name}}:main"
-                     ''')
+    [project.scripts]
+    {{name}} = "{{name}}:main"
+                        ''')
 
-metadata_path = str(os.path.expanduser("~/.METADATA.json"))
-file = open(metadata_path)
-data = json.load(file)
-autor = data["Username"]
-email = data["Email"]
+    metadata_path = str(os.path.expanduser("~/.METADATA.json"))
+    file = open(metadata_path)
+    data = json.load(file)
+    autor = data["Username"]
+    email = data["Email"]
 
 
-out = pp.render({"name":name,"autor":autor,"email":email})
-print(out)
+    out = pp.render({"version":version ,"name":name,"autor":autor,"email":email})
+    print("debug",out)
 
-file_output = open("pyproject.toml","w")
-file_output.write(out)
+    file_output = open("pyproject.toml","w")
+    file_output.write(out)
+
+if __name__ == "__main__":
+    main()
